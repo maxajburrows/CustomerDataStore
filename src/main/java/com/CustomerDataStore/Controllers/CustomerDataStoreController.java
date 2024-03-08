ef2b2b161fa84c29b3c96b10db6d7489001d6999
Max Burrows <maxajburrows@gmail.com>
2024-03-08 19:05:20 +0100

    Set up project structure

---
+++ src/main/java/com/CustomerDataStore/Controllers/CustomerDataStoreController.java
@@ -0,0 +1,7 @@
+package com.CustomerDataStore.Controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomerDataStoreController {
+}
---
+++ src/main/java/com/CustomerDataStore/Services/CustomerDataStoreService.java
@@ -0,0 +1,7 @@
+package com.CustomerDataStore.Services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerDataStoreService {
+}
