70b4fdb702ec39ce6b706df0c4c61e547ad403ae
Max Burrows <maxajburrows@gmail.com>
2024-03-08 18:47:38 +0100

    Create new spring web project

---
+++ src/main/java/com/CustomerDataStore/CustomerDataStoreApplication.java
@@ -0,0 +1,13 @@
+package com.CustomerDataStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerDataStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerDataStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CustomerDataStore/CustomerDataStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CustomerDataStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerDataStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
