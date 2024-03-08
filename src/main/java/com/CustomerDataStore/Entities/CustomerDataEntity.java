fea65269ff5f7bc550edaf5442fa39a154e3323e
Max Burrows <maxajburrows@gmail.com>
2024-03-08 19:27:58 +0100

    Edit entity object

--- src/main/java/com/CustomerDataStore/Entities/CustomerDataEntity.java
+++ src/main/java/com/CustomerDataStore/Entities/CustomerDataEntity.java
@@ -8,12 +8,12 @@ public class CustomerDataEntity {
     @Id
     @GeneratedValue
     private long id;
-    @Column
+    @Column(nullable = false, length = 30)
     String firstName;
-    @Column
+    @Column(nullable = false, length = 30)
     String lastName;
-    @Column
+    @Column(nullable = false)
     String address;
-    @Column
+    @Column(nullable = false, length = 100)
     String emailAddress;
 }
