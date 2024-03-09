8cc882457df5bec44e46e15948cbeab1ae471b56
Max Burrows <maxajburrows@gmail.com>
2024-03-09 19:37:33 +0100

    Tidy code

--- src/main/java/com/CustomerDataStore/Services/CustomerDataServiceImpl.java
+++ src/main/java/com/CustomerDataStore/Services/CustomerDataServiceImpl.java
@@ -4,12 +4,10 @@ import com.CustomerDataStore.Dtos.AddCustomerRequestDto;
 import com.CustomerDataStore.Dtos.CustomerResponseDto;
 import com.CustomerDataStore.Entities.CustomerDataEntity;
 import com.CustomerDataStore.Repositories.CustomerDataRepository;
-import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerDataServiceImpl implements CustomerDataService {
-    ModelMapper modelMapper = new ModelMapper();
     CustomerDataRepository customerDataRepo;
     public CustomerDataServiceImpl(CustomerDataRepository customerDataRepo) {
         this.customerDataRepo = customerDataRepo;
