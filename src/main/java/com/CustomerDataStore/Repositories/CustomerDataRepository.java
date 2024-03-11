cd6b2cbd564a22aac50d8be40733c930064532a0
Max Burrows <maxajburrows@gmail.com>
2024-03-11 18:29:24 +0100

    Add behaviour to getAllCustomers for when there are no customers

--- src/main/java/com/CustomerDataStore/Repositories/CustomerDataRepository.java
+++ src/main/java/com/CustomerDataStore/Repositories/CustomerDataRepository.java
@@ -1,9 +1,10 @@
 package com.CustomerDataStore.Repositories;
 
 import com.CustomerDataStore.Entities.CustomerDataEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CustomerDataRepository extends CrudRepository<CustomerDataEntity, Long> {
+public interface CustomerDataRepository extends JpaRepository<CustomerDataEntity, Long> {
 }
