3d53061c42bbfbf459853070777558027024c644
Max Burrows <maxajburrows@gmail.com>
2024-03-12 22:56:21 +0100

    Add TODO to controller

--- src/main/java/com/CustomerDataStore/Controllers/CustomerDataController.java
+++ src/main/java/com/CustomerDataStore/Controllers/CustomerDataController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 // TODO: Pre-load database with some data
+// TODO: Add swagger documentation
 @RestController
 @RequestMapping("/customers")
 public class CustomerDataController {
