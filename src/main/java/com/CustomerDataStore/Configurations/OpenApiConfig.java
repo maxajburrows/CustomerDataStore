58a47656b713939ab3a21dff95b78a900ad9e7dc
Max Burrows <maxajburrows@gmail.com>
2024-03-13 11:31:53 +0100

    Customise swagger configuration

--- src/main/java/com/CustomerDataStore/Configurations/OpenApiConfig.java
+++
@@ -1,9 +0,0 @@
-//package com.CustomerDataStore.Configurations;
-//
-//import org.springframework.context.annotation.Configuration;
-//
-//
-//@Configuration
-//@EnableOpenApi
-//public class OpenApiConfig {
-//}
