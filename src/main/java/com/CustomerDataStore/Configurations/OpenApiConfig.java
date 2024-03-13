cbfc50e6130d20d89d0a06741069175e5423bfef
Max Burrows <maxajburrows@gmail.com>
2024-03-13 11:25:04 +0100

    Add swagger to the project

---
+++ src/main/java/com/CustomerDataStore/Configurations/OpenApiConfig.java
@@ -0,0 +1,9 @@
+//package com.CustomerDataStore.Configurations;
+//
+//import org.springframework.context.annotation.Configuration;
+//
+//
+//@Configuration
+//@EnableOpenApi
+//public class OpenApiConfig {
+//}
